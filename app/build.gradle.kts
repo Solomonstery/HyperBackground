460d24df9c0034bb967f5a902b54cd5362b5c824
Solomonstery <101304805+Solomonstery@users.noreply.github.com>
2026-09-20 10:12:52 +0800

    chore: 回退到 2484923 并升级到 1.4.4-beta5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.ciallo.hyperbackground"
         minSdk = 33
         targetSdk = 35
-        versionCode = 67
-        versionName = "1.4.4-beta4"
+        versionCode = 68
+        versionName = "1.4.4-beta5"
         resourceConfigurations += listOf("en", "zh-rCN")
     }
 

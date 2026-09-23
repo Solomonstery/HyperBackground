85f0e2b84b9aef38bcbf5dfcd09007712a7c1d41
Solomonstery <101304805+Solomonstery@users.noreply.github.com>
2026-09-23 20:57:46 +0800

    chore: release beta10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.ciallo.hyperbackground"
         minSdk = 33
         targetSdk = 35
-        versionCode = 72
-        versionName = "1.4.4-beta9"
+        versionCode = 73
+        versionName = "1.4.4-beta10"
         resourceConfigurations += listOf("en", "zh-rCN")
     }
 

a57eee44d18452e6f1503a275d6543df55bd8649
1812z <2023158207@qq.com>
2026-09-26 12:56:34 +0800

    Release 1.4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.ciallo.hyperbackground"
         minSdk = 33
         targetSdk = 35
-        versionCode = 74
-        versionName = "1.4.4-beta11"
+        versionCode = 75
+        versionName = "1.4.4"
         resourceConfigurations += listOf("en", "zh-rCN")
     }
 
